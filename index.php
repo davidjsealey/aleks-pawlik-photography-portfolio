dcb09b01e4119f1fd1276f1e6392d8e218fee0bd
David J Sealey <david.sealey@soak.co.uk>
2020-09-08 14:29:52 +0100

    Feature: Add 404 options

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php get_header(); ?>
 
-<main>
-    <?php get_template_part('_flex-content/_content'); ?>
-    <p class="text-center">Index file</p>
-</main>
+<?php if (is_404()):?>
+    <div class="error-block">
+        <div class="error-block__inner">
+            <?php the_field('404_message', 'options'); ?>
+        </div>
+    </div>
+<?php else: ?>
+    <main>
+        <?php get_template_part('_flex-content/_content'); ?>
+        <p class="text-center">Index file</p>
+    </main>
+<?php endif; ?>
 
 <?php get_footer(); ?>
