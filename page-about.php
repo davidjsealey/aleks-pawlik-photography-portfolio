0de24a694588f4f82d361910de4fe566a0887964
David J Sealey <david.sealey@soak.co.uk>
2020-09-10 13:55:39 +0100

    Feature: Grid layout

--- page-about.php
+++ page-about.php
@@ -10,9 +10,11 @@
     </div>
 <?php else: ?>
     <main>
-        <div class="about">
+        <section class="about container">
             <div class="about__wrap">
-                <img src="" alt="" class="about__image">
+                <div class="about__image">
+                    <img src="/wp-content/themes/aleks-pawlik-photography-portfolio/images/placeholder-about.jpg" alt="Aleks Pawlik" class="about__image-src">
+                </div>
                 <div class="about__text">
                     <div class="about__text-container">
                         <h1 class="about__text-header">About me</h1>
@@ -44,7 +46,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     </main>
 <?php endif; ?>
 
