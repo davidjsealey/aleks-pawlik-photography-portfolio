0c1a3973025cfe338ea055b2328ff48f44e8e9b5
David J Sealey <thedavidjsealey@gmail.com>
2021-08-15 10:21:31 +0100

    Feature: New hover effect on slider item

--- _flex-content/_page/carousel.php
+++ _flex-content/_page/carousel.php
@@ -1,8 +1,9 @@
 <?php
+    $hoverEffect = get_sub_field('hover_effect');
     $carousel = get_sub_field('carousel_shortcode');
 ?>
 <section class="carousel">
-    <div class="carousel__container">
+    <div class="carousel__container<?php if ($hoverEffect === 'on') : ?> carousel__container--hover<?php endif; ?>">
         <?php echo $carousel; ?>
     </div>
 </section>
