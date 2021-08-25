ca8dfbb9dc034436caeb1d63c13d23e9ae2d22bb
David J Sealey <thedavidjsealey@gmail.com>
2021-08-25 19:00:29 +0100

    Feature: Add Horizon font for carousel titles

--- webpack.config.js
+++ webpack.config.js
@@ -72,28 +72,16 @@ module.exports = {
         ],
       },
       {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: "url-loader?limit=10000&mimetype=application/font-woff",
+            loader: 'file-loader',
             options: {
-              name: "[name].[hash].[ext]",
-              outputPath: "fonts",
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[hash].[ext]",
-              outputPath: "fonts",
-            },
-          },
-        ],
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       },
       {
         test: /\.jsx$/,
